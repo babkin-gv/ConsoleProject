0ce04adf6f2c38f2dee5890e0c893ebc009dda45
Babkin Gennadiy <babkin@kursksu.ru>
2023-01-08 22:55:39 +0300

    Создан массив

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number: ");
         int num = in.nextInt();
+        int[] arr = new int[num];
+        for (int i=0; i<num;i++){
+            System.out.print("Get a number: ");
+            arr[i] = in.nextInt();
+        }
         System.out.printf("Your number: %d \n", num);
         in.close();
+
     }
 }
